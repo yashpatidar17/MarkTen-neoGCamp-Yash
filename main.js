0532a9f33a0ab9f7de20488b4d42632300b95b37
Yash Patidar <yashpatidar964@gmail.com>
2023-02-07 01:22:54 +0530

    logical part added

--- main.js
+++ main.js
@@ -8,9 +8,20 @@ checkButton.addEventListener("click",validateAmount);
 function validateAmount(){
     errorMessage.style.display = "none";
     if(billAmount>0){
+        if(cashGiven.value > billAmount.value){
+            const amountToReturn = billAmount.value - cashGiven.value;
+            calculateChange(amountToReturn);
+        }else{
+            errorMessage.style.display = "block";
+        errorMessage.innerText = "The cash amount should be greator than bill amount";
+        }
 
     }else{
         errorMessage.style.display = "block";
         errorMessage.innerText = "Please enter bill amount greator than 0";
+    }
+
+    function calculateChange(){
+        
     }
 }
