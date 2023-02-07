f9d40e05c99962e20dbfe6e69da523697070b0c1
Yash Patidar <yashpatidar964@gmail.com>
2023-02-08 02:05:54 +0530

    logic part

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ const billAmount = document.querySelector("#billAmount");
 const cashGiven = document.querySelector("#givenCash");
 const checkButton = document.querySelector("#checkButton");
 const errorMessage = document.querySelector("#ErrorMessage")
-
+const noOfNotes = document.querySelectorAll(".noOfNotes");
+//var Yash = document.querySelector(".yash");
+// console.log(Yash.value);
+console.log(cashGiven.value);
+console.log(billAmount.value);
+// console.log("hello")
+const avaNotes = [2000,500,100,20,10,5,1];
 checkButton.addEventListener("click",validateAmount);
 
 function validateAmount(){
     errorMessage.style.display = "none";
-    if(billAmount>0){
+    if(billAmount.value>0){
         if(cashGiven.value > billAmount.value){
-            const amountToReturn = billAmount.value - cashGiven.value;
+            const amountToReturn = cashGiven.value - billAmount.value;
             calculateChange(amountToReturn);
         }else{
             errorMessage.style.display = "block";
@@ -21,7 +27,11 @@ function validateAmount(){
         errorMessage.innerText = "Please enter bill amount greator than 0";
     }
 
-    function calculateChange(){
-        
+    function calculateChange(amountToReturn){
+        for(let i=0;i<avaNotes.length;i++){
+            const numberOfNotes = Math.trunc(amountToReturn/avaNotes[i]);
+            amountToReturn %= avaNotes[i];
+            noOfNotes[i].innerText = numberOfNotes;
+        };
     }
 }
